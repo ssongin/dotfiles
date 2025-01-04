60147b7a5cfef3d66b9187d971473c87835d0e21
YetAnotherGitUser <ssongin@gmail.com>
2025-01-04 22:57:25 +0200

    Separate oh-my-zsh from zsh

--- copy_all.sh
+++ copy_all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-stow_folders=(  alacritty hyprland i3 kitty lazyvim picom polybar rofi starship tmux 
-                waybar wofi pomodoro xdg zsh private )
+stow_home_folders=(  alacritty hyprland i3 kitty lazyvim picom polybar rofi 
+                starship tmux waybar wofi pomodoro xdg zsh private )
 
 link_files () {
     echo -e "\nLinking $1\n"
-    stow --target="$HOME" $1
+    stow --target=$2 $1
 }
 
-for i in "${stow_folders[@]}"
+for i in "${stow_home_folders[@]}"
 do
-	link_files $i
+	link_files $i $HOME
 done
+
+link_files "oh-my-zsh" "$HOME/.oh-my-zsh"

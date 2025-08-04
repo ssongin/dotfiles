aed99815835c8796c7ed811111c9351123c75858
Stanislav Songin <ssonign@gmail.com>
2025-08-04 17:25:09 +0300

    Add wezterm config

--- copy_all.sh
+++ copy_all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-stow_home_folders=(  alacritty hyprland i3 kitty lazyvim picom polybar rofi 
-                starship tmux waybar wofi pomodoro xdg zsh private )
+stow_home_folders=(alacritty hyprland i3 kitty lazyvim picom polybar rofi
+  starship tmux waybar wezterm wofi pomodoro xdg zsh private)
 
-link_files () {
-    echo -e "\nLinking $1\n"
-    stow --target=$2 $1
+link_files() {
+  echo -e "\nLinking $1\n"
+  stow --target=$2 $1
 }
 
-for i in "${stow_home_folders[@]}"
-do
-	link_files $i $HOME
+for i in "${stow_home_folders[@]}"; do
+  link_files $i $HOME
 done
 
 link_files "oh-my-zsh" "$HOME/.oh-my-zsh"
+
